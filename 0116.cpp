d0f245bc3a1df79a502149ea0baeec02099d77ce
Kuladmir <109757921+Kuladmir@users.noreply.github.com>
2022-07-23 15:58:18 +0800

    Add files via upload

---
+++ 0116.cpp
@@ -0,0 +1,64 @@
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	printf("输入1代表1模式，输入2代表2模式\n");
+	scanf("%d",&a);
+	if(a==1)
+	{
+		printf("输入星期数\n");
+		scanf("%d",&b);
+		switch(b)
+		{
+		case 1:
+			printf("星期一\n");
+			break;
+		case 2:
+			printf("星期二\n");
+			break;
+		case 3:
+			printf("星期三\n");
+			break;
+		case 4:
+			printf("星期四\n");
+			break;
+		case 5:
+			printf("星期五\n");
+			break;
+		case 6:
+			printf("星期六\n");
+			break;
+		case 7:
+			printf("星期天\n");
+			break;
+		default:
+			printf("输入错误\n");
+			break;
+		}
+	}
+	else
+	{
+		printf("输入星期数\n");
+		scanf("%d",&b);
+		switch(b)
+		{
+		case 1:
+		case 2:
+		case 3:
+		case 4:
+		case 5:
+			printf("工作日\n");
+			break;
+		case 6:
+		case 7:
+			printf("休息日\n");
+			break;
+		default:
+			printf("输入错误\n");
+			break;
+		}
+	}
+	return 0;
+}
+
+
---
+++ 0117.cpp
@@ -0,0 +1,60 @@
+#include<stdio.h>
+int main()
+{
+	double a,b,c;
+	int d,e,g;
+	char f;
+	printf("输入计算方式：+ - * / !\n");
+	scanf("%c",&f);
+	switch(f)
+	{
+	case '+':
+		{
+			printf("输入加数\n");
+			scanf("%lf%lf",&a,&b);
+			c=a+b;
+			printf("%lf\n",c);
+		}break;
+	case '-':
+		{
+			printf("输入减数\n");
+			scanf("%lf%lf",&a,&b);
+			c=a-b;
+			printf("%lf\n",c);
+		}break;
+	case '*':
+		{
+			printf("输入乘数\n");
+			scanf("%lf%lf",&a,&b);
+			c=a*b;
+			printf("%lf\n",c);
+		}break;
+	case '/':
+		{
+		printf("输入除数\n");
+		scanf("%lf%lf",&a,&b);
+		if(b==0)
+			printf("error\n");
+		else
+		{
+			c=a/b;
+			printf("%lf\n",c);
+		}
+		}break;
+	case '!':
+		{
+			printf("输入要计算的数\n");
+			scanf("%d",&e);
+			for(g=2,d=1;g<=e;g++)
+			{
+				d=d*g;
+			}
+			printf("%d\n",d);
+		}break;
+	default :
+		printf("error\n");
+		break;
+	}
+	return 0;
+}
+
