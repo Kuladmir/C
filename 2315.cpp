f86dc723c972f2bf2a241b1f935add1240374dd1
Kuladmir <109757921+Kuladmir@users.noreply.github.com>
2022-07-24 10:17:55 +0800

    Add files via upload

---
+++ 2315.cpp
@@ -0,0 +1,78 @@
+#include<stdio.h>
+#include<windows.h>
+#include<conio.h>
+void color(short x)
+{
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),11);
+}
+int main()
+{
+	color(11);
+	printf("					Take me hand\n");
+    printf("In my dreams\n");
+	printf("I feel your light\n");
+	printf("I feel love is born again\n");
+	printf("Fireflies\n");
+	printf("In the moonlight\n");
+	printf("Raising stars\n");
+	printf("Remember the day\n");
+	printf("I fell in love with you\n");
+	printf("Darling won`t you break my heart\n");
+	printf("Take my hand now\n");
+	printf("Stay close to me\n");
+	printf("Be my lover\n");
+	printf("Won`t you let me go\n");
+	printf("Close you eyes now\n");
+	printf("And you will see\n");
+	printf("There`s a rainbow\n");
+	printf("For you and me\n");
+	printf("As I wake up\n");
+	printf("I see your face\n");
+	printf("I feel love is born again\n");
+	printf("Cherry blossom\n");
+	printf("Flying birds\n");
+	printf("In the sky\n");
+	printf("Can`t you see\n");
+	printf("The sun\n");
+	printf("That is shining on the fields\n");
+	printf("Is it shining in your heart\n");
+	printf("Take my hand now\n");
+	printf("Stay close to me\n");
+	printf("Be my lover\n");
+	printf("Won`t you let me go\n");
+	printf("Close your eyes now\n");
+	printf("And you will see\n");
+	printf("There`s a rainbow\n");
+	printf("For you and me\n");
+	printf("And I dream of you\n");
+	printf("Every night\n");
+	printf("Cause there`s only you\n");
+	printf("In my mind\n");
+	printf("Will you be\n");
+	printf("A stranger or a friend in my life\n");
+	printf("Darling won`t you break my heart\n");
+	printf("Take my hand now\n");
+	printf("Stay close to me\n");
+	printf("Be my lover\n");
+	printf("Won`t you let me go\n");
+	printf("Close you eyes now\n");
+	printf("And you will see\n");
+	printf("There`s a rainbow\n");
+	printf("For you and me\n");
+	printf("Take my hand now\n");
+	printf("Stay close to me\n");
+	printf("Be my lover\n");
+	printf("Won`t you let me go\n");
+	printf("Close your eyes now\n");
+	printf("And you will see\n");
+	printf("There`s a rainbow\n");
+	printf("For you and me\n");
+	printf("Darling take my hand now\n");
+	printf("Won`t you stay close to me\n");
+	printf("Won`t you be mu lover\n");
+	printf("Won`t you let me go\n");
+	return 0;
+}
+
+
+
