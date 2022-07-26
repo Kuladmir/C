dfb28c00ac271a10665944198698cb5d301fb8db
Kuladmir <109757921+Kuladmir@users.noreply.github.com>
2022-07-26 16:20:56 +0800

    Add files via upload

---
+++ 0122.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+	int i=0;
+	do 
+	{
+		printf("%d ",i);
+		i++;
+	}while(i<10);
+	printf("\n");
+	for(i=1;i<11;i++)
+	{
+		printf("%d ",i);
+	}
+	printf("\n");
+	return 0;
+}
+
+
