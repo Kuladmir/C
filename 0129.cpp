583c3fe37bb0838219f39de0142eeb60631ad6e0
Kuladmir <109757921+Kuladmir@users.noreply.github.com>
2022-07-28 16:30:08 +0800

    Add files via upload

---
+++ 0129.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>//输出1000-2000之间的闰年
+int main()
+{
+	int i;
+	for(i=1000;i<=2000;i+=4)
+	{
+		if(i%4==0)
+			if(i%100!=0)
+				printf("%d ",i);
+			else if(i%400==0)
+				printf("*%d ",i);
+	}
+	printf("\n");
+	return 0;
+}
+
+
