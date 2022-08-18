12c8dd3fcb8c1a828d16adc0917353d8e7925ef5
Kuladmir <109757921+Kuladmir@users.noreply.github.com>
2022-08-18 21:28:02 +0800

    Add files via upload

---
+++ 0047.cpp
@@ -0,0 +1,30 @@
+#include<stdio.h>//∫Ø ˝ µœ÷√∞≈›≈≈–Ú
+void compare(int a[])
+{
+	int i,j,t;
+	for(i=0;i<9;i++)
+	{
+		for(j=0;j<9-i;j++)
+		{
+			if(a[j]>a[j+1])
+			{
+				t=a[j];
+				a[j]=a[j+1];
+				a[j+1]=t;
+			}
+		}
+	}
+	for(i=0;i<10;i++)
+		printf("%d ",a[i]);
+	printf("\n");
+}
+
+int main()
+{
+	int a[10];
+	int i;
+	for(i=0;i<10;i++)
+		scanf("%d",&a[i]);
+	compare(a);
+	return 0;
+}
