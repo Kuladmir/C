a826690c98be75a8345a1926ca2a0284774c39d7
Kuladmir <109757921+Kuladmir@users.noreply.github.com>
2022-09-05 19:49:15 +0800

    Add files via upload

--- 0089.cpp
+++ 0089.cpp
@@ -1,1 +1,12 @@
 #define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<string.h>
+int  main()
+{
+	char a[20];
+	printf("ÇëÊäÈëÒ»¸ö×Ö·û´®:>");
+	gets_s(a);
+	memset(a, '*', 3);
+	printf("%s\n", a);
+	return 0;
+}
