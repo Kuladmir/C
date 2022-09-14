10ccbfcf8899f7913e94b7c3329ab6c4349b8357
Kuladmir <109757921+Kuladmir@users.noreply.github.com>
2022-09-14 21:18:51 +0800

    Add files via upload

---
+++ 0146.cpp
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<assert.h>
+char* my_strcat(char* a, const char* b)
+{
+	assert(a != NULL);
+	assert(b != NULL);
+	char* p = a;
+	while (*a != '\0')
+		a++;
+	while (*a++ = *b++)
+		;
+	return p;
+}
+int main()
+{
+	char a[20], b[20];
+	printf("请输入一个小于10的字符串:>");
+	gets_s(a);
+	printf("请输入一个小于10的字符串:>");
+	gets_s(b);
+	char*p = my_strcat(a, b);
+	printf("%s\n", p);
+	return 0;
+}
