46aa995e738f6d7de80b833f18cb28608bf8b364
Kuladmir <109757921+Kuladmir@users.noreply.github.com>
2022-10-11 23:38:28 +0800

    Add files via upload

---
+++ 2370.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	printf("please input a number that you like the best\n");
+	scanf("%d",&a);
+	if(a>=610)
+		printf("，-，，-，，-，，-，昆嚴弌砥欣，-，，-，，-，，-，\n");
+	if(a<610)
+		printf("，-，，-，，-，，-，昆嚴寄砥欣，-，，-，，-，，-，\n");
+	return 0;
+}
+	
