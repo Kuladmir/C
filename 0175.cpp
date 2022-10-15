e29f35b7e867fe9435c9a955f699fb576ab364a2
Kuladmir <109757921+Kuladmir@users.noreply.github.com>
2022-10-15 10:06:31 +0800

    Add files via upload

---
+++ 0175.cpp
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<assert.h>
+int my_strcmp(const char* a, const char* b)
+{
+	while (*a == *b)
+	{
+		if (*a == '\0')
+			return 0;
+		a++;
+		b++;
+	}
+	if (*a > *b)
+		return 1;
+	else
+		return -1;
+}
+int main()
+{
+	char a[20], b[20];
+	printf("ÇëÊäÈë:>");
+	gets_s(a);
+	printf("ÇëÊäÈë:>");
+	gets_s(b);
+	int ret = my_strcmp(b, a);
+	if (ret > 0)
+		printf("a<b\n");
+	else if (ret < 0)
+		printf("a>b\n");
+	else
+		printf("a==b\n");
+	return 0;
+}
