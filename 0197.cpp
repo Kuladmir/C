d0e594df0e43c8f934365016e411330845aa025e
Kuladmir <109757921+Kuladmir@users.noreply.github.com>
2022-11-05 18:17:17 +0800

    Add files via upload

---
+++ 0197.cpp
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+    int n, i;
+    printf("«Î ‰»Î’˚ ˝£∫");
+    scanf("%d", &n);
+    printf("%d=", n);
+    for (i = 2; i <= n; i++)
+    {
+        while (n % i == 0)
+        {
+            printf("%d", i);
+            n /= i;
+            if (n != 1) printf("*");
+        }
+    }
+
+    printf("\n");
+    return 0;
+}
