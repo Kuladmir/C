bc33141270688e55901d871b353030aa0403320d
Kuladmir <109757921+Kuladmir@users.noreply.github.com>
2022-11-13 18:08:08 +0800

    Add files via upload

---
+++ 0216.cpp
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+	int a[10],tmp;
+	int i,j;
+	for (i = 0; i < 10; i++)
+		scanf("%d", &a[i]);
+	for (i = 0; i < 9; i++)
+	{
+		for (j = 0; j < 9-i; j++)
+		{
+			if (a[j] < a[j + 1])
+			{
+				tmp = a[j];
+				a[j] = a[j + 1];
+				a[j + 1] = tmp;
+			}
+		}
+	}
+	for (i = 0; i < 10; i++)
+		printf("%d ", a[i]);
+	printf("\n");
+	return 0;
+}
