45e50acfb9c52c2952437e5f72612becb610e79a
Kuladmir <109757921+Kuladmir@users.noreply.github.com>
2022-11-20 22:41:47 +0800

    Add files via upload

--- 2424.cpp
+++ 2424.cpp
@@ -3,36 +3,39 @@
 #define PI 3.14 //定义Π
 #define D 0.042 //定义压强表内径 m
 #define d 0.036 //定义真空表内径 m
-#define Rou 1000 //定义水的密度 kg/m^3
+//#define Rou 1000 //定义水的密度 kg/m^3
 #define h 0.260 //定义高度差 m
 #define g 9.81 //定义重力加速度 N/kg
 int main()
 {
 	printf("			本程序仅可用来计算实验3测定数据\n");
 	printf("这是本程序默认采取的常数：\n");
-	printf("Π = %.2lf   压强表内径D = %.3lfm  真空表内径d = %.3lfm   水的密度ρ= %dkg/m^3   高度差h = %.2lfm   重力加速度g = %.2lfN/kg\n", PI, D, d, Rou, h, g);
+	printf("Π = %.2lf   压强表内径D = %.3lfm  真空表内径d = %.3lfm   高度差h = %.2lfm   重力加速度g = %.2lfN/kg\n", PI, D, d, h, g);
 	printf("如需修改，请联系管理员\n");
 	printf("注意：P出指压力表读数，P入指真空表读数，计算时要取负，注意零点读数\n");
 	double a[30] = { 0 }, b[30] = { 0 }, c[30] = { 0 }, e[30] = { 0 }, f[30] = { 0 }, k[30] = { 0 }, l[30] = {0};
 	int i, j,cho;
 	double x[30] = { 0 }, y[30] = {0};
+	double Rou;
 	do
 	{
 		printf("请输入要计算的组数（1-30），输入0则退出\n");
-		scanf("%d", &i);
+		scanf("%d", &i);		
+		printf("请输入实验温度下流体密度(kg/m3):>");
+		scanf("%lf", &Rou);
 		if (i != 0)
 		{
-			printf("你希望计算第一部分数据，还是第二部分，前者请输入1，后者请输入2\n");
+			printf("你希望计算离心泵曲线数据，还是管路曲线数据，前者请输入1，后者请输入2\n");
 			scanf("%d", &cho);
 			if (cho == 2)
 			{
-				printf("请依次输入 %d 组 P出 的数据:>", i);
+				printf("请依次输入 %d 组 P出 的数据(MPa):>", i);
 				for (j = 0; j < i; j++)
 					scanf("%lf", &a[j]);//接收p出
-				printf("请依次输入 %d 组 P入 的数据:>", i);
+				printf("请依次输入 %d 组 P入 的数据(MPa):>", i);
 				for (j = 0; j < i; j++)
 					scanf("%lf", &b[j]);//接收p入
-				printf("请依次输入 %d 组的流量Q:>", i);
+				printf("请依次输入 %d 组的流量Q(m3/h):>", i);
 				for (j = 0; j < i; j++)
 					scanf("%lf", &c[j]);//接收流量Q
 				for (j = 0; j < i; j++)
@@ -48,16 +51,16 @@ int main()
 			}
 			else
 			{
-				printf("请依次输入 %d 组 P出 的数据:>", i);
+				printf("请依次输入 %d 组 P出 的数据(Mpa):>", i);
 				for (j = 0; j < i; j++)
 					scanf("%lf", &a[j]);//接收p出
-				printf("请依次输入 %d 组 P入 的数据:>", i);
+				printf("请依次输入 %d 组 P入 的数据(Mpa):>", i);
 				for (j = 0; j < i; j++)
 					scanf("%lf", &b[j]);//接收p入
-				printf("请依次输入 %d 组的流量Q:>", i);
+				printf("请依次输入 %d 组的流量Q(m3/h):>", i);
 				for (j = 0; j < i; j++)
 					scanf("%lf", &c[j]);//接收流量Q
-				printf("请依次输入 %d 组功率读数:>", i);
+				printf("请依次输入 %d 组功率读数(kw):>", i);
 				for (j = 0; j < i; j++)
 					scanf("%lf", &f[j]);//接收功率表读数
 				printf("\n");
@@ -79,7 +82,6 @@ int main()
 					printf("结果为%lf	\n", l[j]);
 					printf("\n");
 				}
-				printf("\n");
 			}
 		}
 		else
