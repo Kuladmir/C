23cdb8442591c69c77815396252ddd5e6b5c3ce9
Kuladmir <109757921+Kuladmir@users.noreply.github.com>
2022-12-15 18:19:45 +0800

    Add files via upload

---
+++ 0241.cpp
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+	printf("某个公司采用公用电话传递数据，数据是四位的整数，在传递过程中是加密的，加密规则如下： "
+		"每位数字都加上5,然后用和除以10的余数代替该数字，"
+		"再将第一位和第四位交换，第二位和第三位交换。\n");
+	printf("请输入密码:>");
+	int se[4],i;
+	for (i = 0; i < 4; i++)
+		scanf("%d", &se[i]);
+	for (i = 0; i < 4; i++)
+	{
+		se[i] += 5;
+		se[i] = se[i] % 10;
+	}
+	for (i = 0; i < 4; i++)
+		printf("%d ", se[i]);
+	printf("\n");
+	return 0;
+}
