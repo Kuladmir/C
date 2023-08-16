cdc369c9be0d3bae2a8468e56410f788ef876311
Kuladmir <109757921+Kuladmir@users.noreply.github.com>
2023-08-16 17:15:33 +0800

    Add files via upload

---
+++ 0266.cpp
@@ -0,0 +1,24 @@
+#include  <stdio.h>
+//计算第一门课的平均分
+typedef  struct
+{
+    char  num[8];
+    double  score[2];
+}STU;
+double fun(STU  std[], int  n)
+{
+    int   i;
+    /**********found**********/
+    double  sum = 0.0;
+    /**********found**********/
+    for (i = 0; i < 4; i++)
+        /**********found**********/
+        sum += std[i].score[0];
+    return  sum / n;
+}
+main()
+{
+    STU  std[] = { "N1001", 76.5,82.0 ,"N1002", 66.5,73.0,
+             "N1005", 80.5,66.0,"N1006", 81.0,56.0 };
+    printf("第1门课程的平均分是：%lf\n", fun(std, 4));
+}
